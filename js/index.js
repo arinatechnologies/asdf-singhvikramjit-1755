d7d03768aceb1947002e871e5cb5a3d46224b391
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 23:10:29 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name": "cloud server",
+        "filename": "icon-server.svg"
+    },
+    {
+        "name": "business dashboard",
+        "filename": "icon-dashboard.svg"
+    },
+    {
+        "name": "secure hosting",
+        "filename": "icon-security.svg"
+    }
+]
