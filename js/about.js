8601b828c053a9497dcb69129c496c6981042802
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 23:10:40 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,9 @@
+[
+  "modern office workspace",
+  "business team meeting",
+  "tech startup founders",
+  "company timeline graphic",
+  "diverse professional team",
+  "mission and values concept",
+  "business handshake meeting"
+]
